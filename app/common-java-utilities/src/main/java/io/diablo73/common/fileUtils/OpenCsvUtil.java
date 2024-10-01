d20631e1307303ab95efafaa07f18938ab073280
Diablo73 <ashul2173@gmail.com>
2024-10-02 01:11:21 +0530

    OpenCsvUtil.readAllLines

---
+++ app/common-java-utilities/src/main/java/io/diablo73/common/fileUtils/OpenCsvUtil.java
@@ -0,0 +1,25 @@
+package io.diablo73.common.fileUtils;
+
+import com.opencsv.CSVReader;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+public class OpenCsvUtil {
+
+	public static List<List<String>> readAllLines(String filePath) {
+		try (CSVReader csvReader = new CSVReader(new FileReader(filePath))) {
+			List<List<String>> csvContent = new ArrayList<>();
+			for (String[] array : csvReader.readAll()) {
+				csvContent.add(new ArrayList<>(List.of(array)));
+			}
+			return csvContent;
+		} catch (Exception e) {
+			log.error(String.valueOf(e));
+		}
+		return new ArrayList<>();
+	}
+}
