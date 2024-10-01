414d497d4472b8f4b2ab8eb8981e6758bd95d753
Diablo73 <ashul2173@gmail.com>
2024-10-01 16:15:34 +0530

    BinaryTree

---
+++ app/common-java-utilities/src/main/java/io/diablo73/common/dataStructures/BinaryTree.java
@@ -0,0 +1,108 @@
+package io.diablo73.common.dataStructures;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class BinaryTree<E> {
+
+	private E value;
+	private BinaryTree<E> leftBinaryTree;
+	private BinaryTree<E> rightBinaryTree;
+
+	public BinaryTree() {
+	}
+
+	public BinaryTree(E val) {
+		this.value = val;
+	}
+
+	public BinaryTree(E val, BinaryTree<E> leftBinaryTree, BinaryTree<E> rightBinaryTree) {
+		this.value = val;
+		this.leftBinaryTree = leftBinaryTree;
+		this.rightBinaryTree = rightBinaryTree;
+	}
+
+	public BinaryTree(List<E> arrayList) {
+		if (Objects.nonNull(arrayList) && !arrayList.isEmpty()) {
+			buildTree(arrayList);
+		}
+	}
+
+	public BinaryTree(E[] array) {
+		this(new ArrayList<E>(List.of(array)));
+	}
+
+	public void buildTree(List<E> arrayList) {
+		buildBinaryTree(arrayList);
+//		buildTree(arrayList, 0);
+	}
+
+	private void buildBinaryTree(List<E> arrayList) {
+		this.value = arrayList.get(0);
+		List<BinaryTree<E>> nodeList = new ArrayList<>();
+
+		BinaryTree<E> currentBinaryTree = this;
+		boolean isLeft = true;
+		for (int i = 1; i < arrayList.size(); i++) {
+			if (isLeft) {
+				if (Objects.nonNull(arrayList.get(i))) {
+					currentBinaryTree.leftBinaryTree = new BinaryTree<>(arrayList.get(i));
+					nodeList.add(currentBinaryTree.leftBinaryTree);
+				}
+			} else {
+				if (Objects.nonNull(arrayList.get(i))) {
+					currentBinaryTree.rightBinaryTree = new BinaryTree<>(arrayList.get(i));
+					nodeList.add(currentBinaryTree.rightBinaryTree);
+					currentBinaryTree = nodeList.remove(0);
+				}
+			}
+			isLeft = !isLeft;
+		}
+	}
+
+//	private void buildTree(List<E> arrayList, int index) {
+//		if (index < arrayList.size() && Objects.nonNull(arrayList.get(index))) {
+//			this.value = arrayList.get(index);
+//			int leftIndex = 2 * index + 1;
+//			int rightIndex = 2 * index + 2;
+//
+//			if (leftIndex < arrayList.size() && Objects.nonNull(arrayList.get(leftIndex))) {
+//				this.leftBinaryTree = new BinaryTree<E>();
+//				this.leftBinaryTree.buildTree(arrayList, leftIndex);
+//			}
+//
+//			if (rightIndex < arrayList.size() && Objects.nonNull(arrayList.get(rightIndex))) {
+//				this.rightBinaryTree = new BinaryTree<E>();
+//				this.rightBinaryTree.buildTree(arrayList, rightIndex);
+//			}
+//		}
+//	}
+
+	public E getValue() {
+		return value;
+	}
+
+	public void setValue(E val) {
+		value = val;
+	}
+
+	public BinaryTree<E> getLeftBinaryTree() {
+		return leftBinaryTree;
+	}
+
+	public BinaryTree<E> getRightBinaryTree() {
+		return rightBinaryTree;
+	}
+
+	public int getMaxHeight() {
+		if (Objects.isNull(value)) {
+			return 0;
+		}
+
+		int leftHeight = Objects.nonNull(getLeftBinaryTree()) ? getLeftBinaryTree().getMaxHeight() : 0;
+		int rightHeight = Objects.nonNull(getRightBinaryTree()) ? getRightBinaryTree().getMaxHeight() : 0;
+
+		return Math.max(leftHeight, rightHeight) + 1;
+	}
+}
